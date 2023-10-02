eaecf305a615107853fa8b792d5c9260c295c669
viseliza <nenyxen123@gmail.com>
2023-10-02 08:02:59 +0300

    that trash...

--- src/apps/home_page/apps.py
+++ src/apps/home_page/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class HomePageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'home_page'
+    name = 'apps.home_page'
--- src/config/urls.py
+++ src/config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home_page.urls'))
+    path('', include('apps.home_page.urls'))
 ]
